dccf32fdf4bea6664785b9bf04b20b9965b8740e
Sianna Puente <spuente@cellforce.com>
2021-05-25 00:58:10 -0500

    Add binding change color in button operation

--- CalculatorSwiftUI/ContentView.swift
+++ CalculatorSwiftUI/ContentView.swift
@@ -18,16 +18,22 @@ struct ContentView: View {
         result: "0"
     )
     
+    @State var pressAction: Bool = false
     
     var contentButtons: [[MButton]] = [
-        [MButton("A/C", .gray, operation: .ac), MButton("+/-", .gray, operation: .sign), MButton("%", .gray, operation: .porcentage), MButton("/", .yellow, operation: .divition)],
-        [MButton("7", operation: .number), MButton("8", operation: .number), MButton("9", operation: .number), MButton("X", .yellow, operation: .multiply)],
-        [MButton("4", operation: .number), MButton("5", operation: .number), MButton("6", operation: .number), MButton("-", .yellow, operation: .substract)],
-        [MButton("1", operation: .number), MButton("2", operation: .number), MButton("3", operation: .number), MButton("+", .yellow, operation: .plus)]
+        [MButton("A/C", .gray, operation: .ac), MButton("+/-", .gray, operation: .sign), MButton("%", .gray, operation: .porcentage),
+         MButton("/", .yellow, operation: .divition)],
+        [MButton("7", operation: .number), MButton("8", operation: .number), MButton("9", operation: .number),
+         MButton("X", .yellow, operation: .multiply)],
+        [MButton("4", operation: .number), MButton("5", operation: .number), MButton("6", operation: .number),
+         MButton("-", .yellow, operation: .substract)],
+        [MButton("1", operation: .number), MButton("2", operation: .number), MButton("3", operation: .number),
+         MButton("+", .yellow, operation: .plus)]
     ]
     
     
     var body: some View {
+        
         VStack{
             Spacer(minLength: 120)
             Text("\(result)")
@@ -41,26 +47,25 @@ struct ContentView: View {
                     HStack{
                         ForEach(0..<contentButtons[posx].count) { posy in
                             ButtonView(
-                                content: contentButtons[posx][posy].getView(),
-                                operation: contentButtons[posx][posy].operation,
+                                mButton: contentButtons[posx][posy],
                                 modelAction: $modelAction,
-                                result: $result)
+                                result: $result,
+                                pressAction: $pressAction)
                         }
                     }
                 }
                 HStack{
-                    ButtonView(content:MButton("0", .double, operation: .number).getView(),
-                               operation: .number,
-                               modelAction: $modelAction, result: $result)
+                    ButtonView(mButton:MButton("0", .double, operation: .number),
+                               modelAction: $modelAction, result: $result, pressAction: $pressAction)
                     HStack{
-                        ButtonView(content: MButton(".", operation: .comma).getView(),
-                                   operation: .comma,
+                        ButtonView(mButton: MButton(".", operation: .comma),
                                    modelAction: $modelAction,
-                                   result: $result)
-                        ButtonView(content: MButton("=", .yellow, operation: .equal).getView(),
-                                   operation: .equal,
+                                   result: $result,
+                                   pressAction: $pressAction)
+                        ButtonView(mButton: MButton("=", .yellow, operation: .equal),
                                    modelAction: $modelAction,
-                                   result: $result)
+                                   result: $result,
+                                   pressAction: $pressAction)
                     }
                 }
             }
@@ -77,25 +82,44 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct ButtonView: View {
-    var content: ContentButtonView
-    var operation: Operation
+    var mButton: MButton = MButton()
+       // var content: ContentButtonView = TypeButton.main.getContentBV(label: <#T##String#>, keepPress: <#T##Binding<Bool>#>)
+    // var operation: Operation = .zero
     
     @Binding var modelAction: ModelAction
     @Binding var result: String
+    @Binding var pressAction: Bool
+    
+    @State var keepPress: Bool = false
+
     
     
     var body: some View {
         
         Button(action: {
             
+            let operation = mButton.operation
+
+           
+            
+            if mButton.typeButton == .yellow && pressAction == false {
+                keepPress = true
+                pressAction = true
+            }
+            
+            if mButton.typeButton == .main {
+                pressAction = false
+            }
+            
+            
             if modelAction.lastOperation == Operation.zero{
                 
                 if operation != .number {
                     result = "0"
-                } else if content.label == "."  {
+                } else if mButton.text == "."  {
                     result = "0."
                 } else {
-                    result = content.label
+                    result = mButton.text
                     modelAction.setValue(result)
                     modelAction.againOperate()
                 }
@@ -116,7 +140,7 @@ struct ButtonView: View {
                         case .number:
                             
                             if !result.contains(Operation.comma.rawValue) {
-                                result = "\(result)\(content.label)"
+                                result = "\(result)\(mButton.text)"
                                 modelAction.valueB = result
                             }
                             break
@@ -128,7 +152,6 @@ struct ButtonView: View {
                     }
                     
                 } else {
-
                     modelAction.againOperate()
                     result = modelAction.result
                     modelAction.setOperation(operation)
@@ -139,10 +162,10 @@ struct ButtonView: View {
             else if operation == Operation.number && modelAction.lastOperation != Operation.number{
                 
                 if modelAction.lastOperation == .comma {
-                    result = "\(result)\(content.label)"
+                    result = "\(result)\(mButton.text)"
                     modelAction.valueB = result
                 } else {
-                    result = content.label
+                    result = mButton.text
                     modelAction.setValue(result)
                 }
             
@@ -152,14 +175,14 @@ struct ButtonView: View {
             else if operation == Operation.number && modelAction.lastOperation == Operation.number {
                 
                 if result.contains(Operation.comma.rawValue) {
-                    result = "\(result)\(content.label)"
+                    result = "\(result)\(mButton.text)"
                     modelAction.valueB = result
                 } else {
                     if result.first != "0" {
-                        result = "\(result)\(content.label)"
+                        result = "\(result)\(mButton.text)"
                         modelAction.valueB = result
-                    } else if result.first == "0" && content.label != "0"{
-                        result = "\(content.label)"
+                    } else if result.first == "0" && mButton.text != "0"{
+                        result = "\(mButton.text)"
                         modelAction.valueB = result
                     }
                 }
@@ -185,8 +208,16 @@ struct ButtonView: View {
             modelAction.lastOperation = operation
             
            
-        }, label: {
-            content
+            
+        }, label: { () -> ContentButtonView in
+            
+            
+            if mButton.typeButton == .main {
+                return mButton.typeButton.getContentBV(label: mButton.text, keepPress: $keepPress)
+            } else {
+                return mButton.typeButton.getContentBV(label: mButton.text, keepPress: pressAction ? $keepPress : $pressAction)
+            }
+            
            
         })
         
@@ -202,6 +233,7 @@ class ModelAction {
     var valueB: String
     var lastOperation: Operation
     var result: String
+    // var btnView: ButtonView
     
     
     init(valueA: String, operation: Operation, valueB: String, lastOperation: Operation, result: String) {
@@ -234,7 +266,6 @@ class ModelAction {
     }
     
     func againOperate() {
-        // operation == .equal ? self.result :
         let valueResult =  operation.operar(a: self.valueA, b: self.valueB) ?? ""
         if (valueResult.isEmpty || valueResult == "ERROR" ){
             self.resetValue()
@@ -250,27 +281,29 @@ struct ContentButtonView: View {
     var color: Color = Color(.black)
     var backgroundColor: Color = Color(.gray)
     var isCircle: Bool = true
+    @Binding var keepPress: Bool
     
     var body: some View {
         if isCircle {
         Text(label)
             .font(.title)
-            .foregroundColor(color)
+            .foregroundColor(keepPress ? backgroundColor : color)
             .frame( maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,  maxHeight: .infinity, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            .background(backgroundColor)
+            .background(keepPress ? color : backgroundColor)
             .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
             .padding(1)
         } else {
             Text(label)
                 .font(.title)
-                .foregroundColor(color)
+                .foregroundColor(keepPress ? backgroundColor : color)
                 .padding(24)
                 .frame( maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-                .background(backgroundColor)
+                .background(keepPress ? color : backgroundColor)
                 .clipShape(Capsule())
                 .padding(1)
         }
     }
+
 }
 
 
@@ -280,49 +313,53 @@ struct MButton {
     var text:String
     var typeButton:TypeButton
     var operation:Operation
+    //@Binding var keepPress: Bool
     
+    init(){
+        self.text = ""
+        self.typeButton = .main
+        self.operation = .zero
+    }
     
     init(_ text:String, _ typeButton:TypeButton = .main, operation: Operation) {
         self.text = text
         self.typeButton = typeButton
         self.operation = operation
+        //self._keepPress = keepPress
     }
     
-    func getView()-> ContentButtonView{
+    /*func getView()-> ContentButtonView{
         return typeButton.getContentBV(label: self.text)
-    }
+    }*/
 }
 
 enum TypeButton {
-    case main
-    case gray
-    case yellow
-    case yellowContrast
-    case double
+    case main // buttons number
+    case gray // buttons extra
+    case yellow // buttons operations
+    case double // button zero
     
-    func getContentBV(label:String)-> ContentButtonView{
+    func getContentBV(label:String, keepPress: Binding<Bool> )-> ContentButtonView{
         switch self {
         case .main:
             return ContentButtonView(label: label,
                               color: Color.white,
-                              backgroundColor: Color(red: 0.2, green: 0.2, blue: 0.2))
+                              backgroundColor: Color(red: 0.2, green: 0.2, blue: 0.2),
+                              keepPress: keepPress)
         case .gray:
-            return ContentButtonView(label: label)
+            return ContentButtonView(label: label,keepPress: keepPress)
             
         case .yellow:
             return ContentButtonView(label: label,
-                              color: Color(.white),
-                              backgroundColor: Color(red: 0.9, green: 0.6, blue: 0.01))
-        case .yellowContrast:
-            return ContentButtonView(label: label,
-                              color: Color(red: 0.9, green: 0.6, blue: 0.01),
-                              backgroundColor: Color(.white))
-                            
+                                     color: Color(.white),
+                                     backgroundColor:  Color(red: 0.9, green: 0.6, blue: 0.01),
+                                     keepPress: keepPress)
         case .double:
             return ContentButtonView(label: label,
                               color: Color(.white),
                               backgroundColor: Color(red: 0.2, green: 0.2, blue: 0.2),
-                              isCircle: false)
+                              isCircle: false,
+                              keepPress: keepPress)
         }
     }
 }
